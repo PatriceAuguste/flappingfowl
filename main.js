a304f0e380c9f4ae03ddd420d84b56df8ac51a13
PatriceAuguste <patriceauguste16@gmail.com>
2016-07-15 12:41:32 -0400

    changed the color

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var mainState = {
     },
 
     create: function() {
-        game.stage.backgroundColor = 'navyBlue'
+        game.stage.backgroundColor = 'lightBlue'
         game.physics.startSystem(Phaser.Physics.ARCADE);
         this.bird= game.add.sprite(100,254, 'bird')
         game.physics.arcade.enable(this.bird);
